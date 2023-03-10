4f56cd8513caee40c797d3bc70e2a8f0c78c6520
Bethel Ikonne <46830724+ikonbethel@users.noreply.github.com>
2023-03-10 10:19:52 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,1 +1,53 @@
+#!/usr/bin/python3
+"""Module for TestHBNBCommand class."""
 
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+import unittest
+import datetime
+from unittest.mock import patch
+import sys
+from io import StringIO
+import re
+import os
+
+
+class TestHBNBCommand(unittest.TestCase):
+
+    """Tests HBNBCommand console."""
+
+    attribute_values = {
+        str: "foobar108",
+        int: 1008,
+        float: 1.08
+    }
+
+    reset_values = {
+        str: "",
+        int: 0,
+        float: 0.0
+    }
+
+    test_random_attributes = {
+        "strfoo": "barfoo",
+        "intfoo": 248,
+        "floatfoo": 9.8
+    }
+
+    def setUp(self):
+        """Sets up test cases."""
+        if os.path.isfile("file.json"):
+            os.remove("file.json")
+        self.resetStorage()
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def test_help(self):
+        """Tests the help command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+        s = """
